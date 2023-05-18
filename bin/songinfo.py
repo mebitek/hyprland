a32b62fadba98e0adf29bb9ec7547e01930f679f
mebitek <mebitek@zohomail.eu>
2023-05-18 16:44:42 +0200

    fix get image

--- bin/songinfo.py
+++ bin/songinfo.py
@@ -22,21 +22,22 @@ async def main():
         image_url = images[uri][0]['uri']
 
         a = urlparse(image_url)
+        print(a)
         file_name = os.path.basename(a.path)
         tmp_file_name = "/tmp/%s"%file_name
     
         print("%s\n%s - %s\n%s"%(name,artist,album,tmp_file_name))
 
-        if not os.path.isfile(tmp_file_name):
+        #if not os.path.isfile(tmp_file_name):
 
-            if not uri.startswith('local'):
-                response = requests.get(image_url)
-                with open(tmp_file_name, "wb") as f:
-                    f.write(response.content)
-            else:
-                home_dir = os.path.expanduser('~')
-                src_file = r"%s/.local/share/mopidy/local/images/%s"%(home_dir, file_name)
-                shutil.copyfile(src_file, tmp_file_name)
+        if not uri.startswith('local'):
+           response = requests.get(image_url)
+           with open(tmp_file_name, "wb") as f:
+               f.write(response.content)
+        else:
+           home_dir = os.path.expanduser('~')
+           src_file = r"%s/.local/share/mopidy/local/images/%s"%(home_dir, file_name)
+           shutil.copyfile(src_file, tmp_file_name)
 
 asyncio.run(main())
 
