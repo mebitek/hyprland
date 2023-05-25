0f4fbc895804891acbcd64a684fca55a96034620
mebitek <mebitek@zohomail.eu>
2023-05-25 18:01:13 +0200

    fix image when it's not available

--- bin/songinfo.py
+++ bin/songinfo.py
@@ -18,21 +18,24 @@ async def main():
         album = track['album']['name']
 
         images = await mopidy.library.get_images([uri])
-
-        image_url = images[uri][0]['uri']
-
-        a = urlparse(image_url)
-        file_name = os.path.basename(a.path)
-        tmp_file_name = "/tmp/%s"%file_name
+        
+        tmp_file_name ="" 
+        image_url = None
+        if images[uri]:
+            image_url = images[uri][0]['uri']
+            a = urlparse(image_url)
+            file_name = os.path.basename(a.path)
+            tmp_file_name = "/tmp/%s"%file_name
     
         print("%s\n%s - %s\n%s"%(name,artist,album,tmp_file_name))
 
         #if not os.path.isfile(tmp_file_name):
 
         if not uri.startswith('local'):
-           response = requests.get(image_url)
-           with open(tmp_file_name, "wb") as f:
-               f.write(response.content)
+            if image_url:   
+                response = requests.get(image_url)
+                with open(tmp_file_name, "wb") as f:
+                    f.write(response.content)
         else:
            home_dir = os.path.expanduser('~')
            src_file = r"%s/.local/share/mopidy/local/images/%s"%(home_dir, file_name)
--- bin/songinfo.sh
+++ bin/songinfo.sh
@@ -5,5 +5,9 @@ REQ=$($HOME/bin/songinfo.py)
 TEXT=$(echo "${REQ}" | grep -v tmp)
 FILE_NAME=$(echo "${REQ}" | grep tmp)
 
-notify-send -r 27072 "Now Playing" "$TEXT" -i $FILE_NAME
-kitty @ set-background-image $FILE_NAME
+if [[ $FILE_NAME != "" ]]; then
+	notify-send -r 27072 "Now Playing" "$TEXT" -i $FILE_NAME
+	kitty @ set-background-image $FILE_NAME
+else
+	notify-send -r 27072 "Now Playing" "$TEXT"
+fi
