3fa3a789308ac0b57c0c1256bb9b1c945577c98e
mebitek <mebitek@zohomail.eu>
2023-06-20 14:48:16 +0200

    add mpd next button shortcut

--- waybar/bin/mpd_control.sh
+++ waybar/bin/mpd_control.sh
@@ -2,6 +2,14 @@
 
 PLAYING_STATUS=$(mpc status 2>/dev/null | awk 'FNR==2 {print $1}' RS='[' FS=']')
 
+if [[ -n $1 && $1 == "next" ]]; then
+	echo "next song"
+	mpc next 2>/dev/null
+	exit
+elif [[ -n $1 ]]; then
+	exit
+fi
+
 if [[ $PLAYING_STATUS == "playing" ]]; then
 	echo "set to pause"
 	mpc pause 2>/dev/null
