2b34240e924d62e7806437513f964f12ff97b5af
mebitek <mebitek@zohomail.eu>
2026-04-23 12:08:41 +0200

    fix music control to jellyfin-tui

--- waybar/bin/mpd_control.sh
+++ waybar/bin/mpd_control.sh
@@ -5,7 +5,7 @@ PLAYING_STATUS=$(playerctl -p jellyfin-tui status)
 
 if [[ -n $1 && $1 == "next" ]]; then
   echo "next song"
-  playerctl next 2>/dev/null
+  playerctl -p jellyfin-tui next 2>/dev/null
   exit
 elif [[ -n $1 ]]; then
   exit
@@ -13,8 +13,8 @@ fi
 
 if [[ $PLAYING_STATUS == "Playing" ]]; then
   echo "set to pause"
-  playerctl pause 2>/dev/null
+  playerctl -p jellyfin-tui pause 2>/dev/null
 else
   echo "set to play"
-  playerctl play 2>/dev/null
+  playerctl -p jellyfin-tui play 2>/dev/null
 fi
